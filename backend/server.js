1fc3385b6791754250f141ee822f24c2f5107540
burhancoulibaly <burhancoulibaly@gmail.com>
2019-10-15 16:34:28 -0400

    made some minor html changes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const express = require("express"),
       path = require("path"),
       server = require("http").createServer(app);
 
-var main = path.resolve("./frontend/html/home.html"),
-    css = path.resolve("./frontend/css"),
-    js = path.resolve("./frontend/js"),
-    p5js = path.resolve("./node_modules/p5/lib");
+      main = path.resolve("./frontend/html/home.html"),
+      css = path.resolve("./frontend/css"),
+      js = path.resolve("./frontend/js"),
+      p5js = path.resolve("./node_modules/p5/lib");
 
 app.use("/main", express.static(main));
 app.use("/css", express.static(css));
